8deb47e76eec4f4d9816548119892574cd44e893
SangkiHan <68369248+SangkiHan@users.noreply.github.com>
2023-06-23 14:15:55 +0900

    feat: 집합, 그룹 테스트코드

--- src/test/java/com/spring/jpa/querydsl/QueryDslApplicationTests.java
+++ src/test/java/com/spring/jpa/querydsl/QueryDslApplicationTests.java
@@ -10,9 +10,11 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Rollback;
 
 import com.querydsl.core.QueryResults;
+import com.querydsl.core.Tuple;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import com.spring.jpa.querydsl.entity.Member;
 
+import static com.spring.jpa.querydsl.entity.QTeam.team;
 import static com.spring.jpa.querydsl.entity.QMember.member;
 
 import java.util.List;
@@ -78,5 +80,61 @@ class QueryDslApplicationTests {
 				.fetchResults();
 		
 	}
-
+	
+	@Test
+	public void sort() {
+		List<Member> results = queryFactory
+					.selectFrom(member)
+					.orderBy(member.age.desc(), member.username.asc().nullsLast())
+					.fetch();
+		
+		System.out.println(results);
+	}
+	
+	@Test
+	public void paging1() {
+		List<Member> results = queryFactory
+					.selectFrom(member)
+					.orderBy(member.age.desc())
+					.offset(1)
+					.limit(2)
+					.fetch();
+		
+		System.out.println(results);
+	}
+	
+	@Test
+	public void aggregation() {
+		Tuple tuple = queryFactory
+				.select(
+						member.count(),
+						member.age.sum(),
+						member.age.avg(),
+						member.age.max(),
+						member.age.min()
+						)
+				.from(member)
+				.fetchOne();
+		
+		Long count = tuple.get(member.count());
+		int sum = tuple.get(member.age.sum());
+		Double avg = tuple.get(member.age.avg());
+		int max = tuple.get(member.age.max());
+		int min = tuple.get(member.age.min());
+	}
+	
+	@Test
+	public void TeamAvg() {
+		List<Tuple> tuples = queryFactory
+				.select(
+						team.name, member.age.avg()
+						)
+				.from(member.team, team)
+				.groupBy(team.id)
+				.fetch();
+		Tuple tuple = tuples.get(0);
+		
+		String count = tuple.get(team.name);
+		double avg = tuple.get(member.age.avg());
+	}
 }
